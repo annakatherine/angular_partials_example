6f48fbd55d8791292829e284743a6ada7fcbec7c
anna baker <annakatherinebaker@gmail.com>
2016-06-24 18:26:24 -0500

    first commit

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,37 @@
 var myApp=angular.module( 'myApp', [] );
 
-myApp.controller( 'fridayController', [ '$scope', function( $scope ){
+myApp.controller( 'assignmentController', [ '$scope', '$http', function( $scope, $http ){
+  $scope.assignmentArray=[];
   // get user input on button click
-  $scope.getUserInput = function()
-  {
-    console.log( 'in getUserInput: ' + $scope.inputModel1 );
+  $scope.addAssignment = function(){
+    event.preventDefault();
+    var assignmentObject = {
+      assignment: $scope.assignmentId,
+      name: $scope.student_name,
+      score: $scope.score,
+      date_completed: $scope.date_completed,
+      completed: $scope.completed
+    };
+    $http({
+      method: 'POST',
+      url: ('/postAssignment'),
+      data: assignmentObject
+    });//end of htt post call
+    $scope.assignmentId='';
+    $scope.student_name='';
+    $scope.score='';
+    $scope.date_completed='';
+    $scope.completed='';
+  };// end addAssignment
+  $scope.getAssignments = function(){
+    $http({
+      method: 'GET',
+      url: ('/getAssignment'),
+    }).then(function (response) {
+      $scope.assignmentArray = response.data;
+    },
+    function myError( response ){
+      console.log( response.statusText );
+    });
   };
-}]);
+}]); //end of controller
--- server/app.js
+++ server/app.js
@@ -2,6 +2,20 @@ var express = require('express');
 var app=express();
 var path = require('path');
 var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/assignments');
+
+var assignmentSchema = new mongoose.Schema( {
+  assignment_number: String,
+  student_name: String,
+  score: String,
+  date_completed: String,
+  completed: String
+});
+
+var assignmentModel = mongoose.model( 'assignmentModel', assignmentSchema);
 
 // server up
 app.listen( 8080, 'localhost', function( req, res ){
@@ -16,3 +30,24 @@ app.get( '/', function ( req, res ){
 
 // static folder
 app.use( express.static( 'public' ) );
+
+app.get( '/getAssignment', function( req, res ){
+  console.log( 'in getassignment get');
+  
+});
+
+app.post( '/postAssignment', function( req, res ){
+  console.log( 'in post assignment ' + req.body.assignment );
+  var assignmentAdded = {
+    assignment_number: req.body.assignment,
+    student_name: req.body.name,
+    score: req.body.score,
+    date_completed: req.body.date_completed,
+    completed: req.body.completed
+ };
+ // MAGIIIIIIIIIIC
+ var assignmentOut = assignmentModel( assignmentAdded );
+ console.log( 'assignmentAdded: ' + assignmentAdded );
+ assignmentOut.save();
+ console.log( 'assignmentOut: ' + assignmentOut );
+});
